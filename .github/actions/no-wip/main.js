90131e077bd56367a9e5020ad6a7dad4930a31c6
James James <jamesjay4199@gmail.com>
2020-03-16 17:41:34 +0100

    WIP PRs

---
+++ .github/actions/no-wip/main.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const {GitHub, context} = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput('repo-token');
+    const octokit = new GitHub(token);
+
+    const {data: PR} = await octokit.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number,
+    });
+    if(PR.title.includes('WIP')) {
+      await octokit.issues.createComment({
+        repo: context.repo.repo,
+        owner: context.repo.owner,
+        body: 'WIP PRs are not allowed. Please modify the PR title or create a draft PR.',
+        issue_number: context.payload.pull_request.number
+      });
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
