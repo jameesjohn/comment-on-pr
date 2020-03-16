211eec39761ecad2ec1109bcbc43e3a83a607c24
James James <jamesjay4199@gmail.com>
2020-03-16 15:25:52 +0100

    changed to issue level comment

--- .github/actions/comment/main.js
+++ .github/actions/comment/main.js
@@ -7,12 +7,11 @@ async function run() {
     const token = core.getInput('repo-token');
     const octokit = new GitHub(token);
 
-    const comment = await octokit.pulls.createComment({
+    const comment = await octokit.issues.createComment({
       repo: context.repo.repo,
       owner: context.repo.owner,
       body: 'This is my comment ooo',
-      commit_id: context.sha,
-      pull_number: context.payload.pull_request.number
+      issue_number: context.payload.pull_request.number
     });
     core.infoconsole.log(comment.data.body)
   } catch(error) {
