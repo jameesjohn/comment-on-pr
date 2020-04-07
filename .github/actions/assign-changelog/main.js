6bd7f33a28542e764c5ba30de221edf085114270
James James <jamesjay4199@gmail.com>
2020-04-07 11:25:46 +0100

    force pushing

--- .github/actions/assign-changelog/main.js
+++ .github/actions/assign-changelog/main.js
@@ -8,7 +8,7 @@ async function run() {
     const token = core.getInput('repo-token');
 
     const octokit = new GitHub(token);
-    // with force push.
+    // with force pushnow.
     const {data: PR} = await octokit.pulls.get({
       owner: context.repo.owner,
       repo: context.repo.repo,
